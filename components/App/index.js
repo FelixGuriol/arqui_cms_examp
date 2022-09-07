3e8be185863a1de7ff0553b20ae4504b3e07371b
Leonard Norton <mrgriffin001001@gmail.com>
2022-09-07 17:19:37 -0500

    add footer

--- components/App/index.js
+++ components/App/index.js
@@ -1,33 +1,37 @@
 import React from "react";
 import Script from "next/script";
 import Header from "../Header";
+import Footer from "../Footer";
 
 import ImageBanner from "../ImageBanner/ImageBanner";
 
 const components = {
-  image_banner: ImageBanner,
+    image_banner: ImageBanner,
 };
 
 function App({ config, extra }) {
-  const header = config.use_header ? (
-    <Header config={config.layout.header}></Header>
-  ) : null;
+    const header = config.use_header ? (
+        <Header config={config.layout.header}></Header>
+    ) : null;
+    const footer = config.use_footer ? (<Footer config={config.layout.footer}></Footer>) : null
+    const generateStructure = (item, index) => {
+        const Component = components[item];
+        if (Component)
+            return <Component key={index} config={config.sections[item]} />;
+        return null;
+    };
+    const bd = config?.order ?(config.order.map((item, index) => generateStructure(item, index))):null 
 
-  const generateStructure = (item, index) => {
-    const Component = components[item];
-    if (Component)
-      return <Component key={index} config={config.sections[item]} />;
-    return null;
-  };
-
-  return (
-    <div>
-      <Script src="https://cdn.tailwindcss.com" />
-      {header}
-      {config.order.map((item, index) => generateStructure(item, index))}
-      <h1 className="text-3l underline">hla dude</h1>
-    </div>
-  );
+    return (
+        <div>
+            <Script src="https://cdn.tailwindcss.com" />
+            {header}
+            <main className="mb-auto ">
+                {bd}
+            </main>
+            {footer}
+        </div>
+    );
 }
 
 export default App;
